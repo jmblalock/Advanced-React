affdb7afacadff333409f6c33db5341dc9bac11f
jmblalock <justin.m.blalock@gmail.com>
2021-04-19 17:03:55 -0500

    created User data type

--- sick-fits/backend/.keystone/admin/pages/_app.js
+++ sick-fits/backend/.keystone/admin/pages/_app.js
@@ -4,9 +4,13 @@ import { KeystoneProvider } from '@keystone-next/admin-ui/context';
 import { ErrorBoundary } from '@keystone-next/admin-ui/components';
 import { Core } from '@keystone-ui/core';
 
+import * as view2009ba71 from '../../../node_modules/@keystone-next/fields/types/mongoId/views';
+import * as view09b28279 from '../../../node_modules/@keystone-next/fields/types/text/views';
+import * as viewc0b6f99f from '../../../node_modules/@keystone-next/fields/types/password/views';
+
 const adminConfig = {};
 
-const fieldViews = {};
+const fieldViews = { view2009ba71, view09b28279, viewc0b6f99f };
 
 const lazyMetadataQuery = {
   kind: 'Document',
@@ -157,7 +161,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="cgdttq"
+        adminMetaHash="1n5pzjj"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,4 +1,5 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 import 'dotenv/config';
 
 const databaseURL =
@@ -23,6 +24,7 @@ export default config({
   },
   lists: createSchema({
     // Schema items go in here
+    User,
   }),
   ui: {
     // TODO: change this for roles
