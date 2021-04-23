3b2355f5af057de61082a49563333c8f2323dba8
jmblalock <justin.m.blalock@gmail.com>
2021-04-23 16:39:33 -0500

    updating items

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ffac9776689b32f77445";
+/******/ 	var hotCurrentHash = "033665fbed2b3fa0def4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
