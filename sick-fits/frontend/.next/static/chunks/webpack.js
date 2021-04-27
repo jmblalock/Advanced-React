a299f45e950f4899b6d44c495712aecd444165a7
jmblalock <justin.m.blalock@gmail.com>
2021-04-26 19:36:49 -0500

    adjusted query for pagination value

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8631543649ae80818181";
+/******/ 	var hotCurrentHash = "d159afc7f614e05b49cd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
