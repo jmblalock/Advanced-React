81edd73d9f169c332ec2b34435df714dc385d548
jmblalock <justin.m.blalock@gmail.com>
2021-04-27 19:22:09 -0500

    added custom type policies and controls over the Apollo cache

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d159afc7f614e05b49cd";
+/******/ 	var hotCurrentHash = "3d32b8c74fce26301ffe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
