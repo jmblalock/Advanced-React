ab434701cd5b39ef5b0f9aa0abbd858d89743318
jmblalock <justin.m.blalock@gmail.com>
2021-04-27 20:36:04 -0500

    querying the current user

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3d32b8c74fce26301ffe";
+/******/ 	var hotCurrentHash = "ff57680bd9a6eb78faae";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
