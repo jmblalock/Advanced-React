f332b057f7820cb2c5867897baa06c1a4b71a956
jmblalock <justin.m.blalock@gmail.com>
2021-04-28 16:45:29 -0500

    created sign-in component

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff57680bd9a6eb78faae";
+/******/ 	var hotCurrentHash = "a1db1d3ecce6118404cd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
