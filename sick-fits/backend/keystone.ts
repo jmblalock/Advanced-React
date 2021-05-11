95e5fa771d091ccba025f95989f972725e3b7a1f
jmblalock <justin.m.blalock@gmail.com>
2021-05-10 19:52:30 -0500

    adding items to cart

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -11,6 +11,7 @@ import { CartItem } from './schemas/CartItem';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -61,6 +62,7 @@ export default withAuth(
       ProductImage,
       CartItem,
     }),
+    extendGraphqlSchema,
     ui: {
       // Show the UI only for people who pass this test
       isAccessAllowed: ({ session }) =>
