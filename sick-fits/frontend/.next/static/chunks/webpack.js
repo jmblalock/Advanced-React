16778ca8303df20dc72119452baabfdb45759936
jmblalock <justin.m.blalock@gmail.com>
2021-05-18 14:14:56 -0500

    writing client side checkout handler logic

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cebff37a95406abd0b41";
+/******/ 	var hotCurrentHash = "53883c1df45a1f349da1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
