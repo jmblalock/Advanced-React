96812d5ffb46a348b6879452e74279af7f4513bb
jmblalock <justin.m.blalock@gmail.com>
2021-05-20 09:04:35 -0500

    linking up frontend to the custom backendcheckout mutuation

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -69,6 +69,7 @@ async function checkout(
       console.log(err);
       throw new Error(err.message);
     });
+  console.log(charge);
   // 4. Convert the cartItems to OrderItems
   // 5. Create the order and return it
 }
--- sick-fits/frontend/components/Checkout.js
+++ sick-fits/frontend/components/Checkout.js
@@ -1,4 +1,4 @@
-import { NodePath } from '@babel/traverse';
+import { useMutation } from '@apollo/client';
 import {
   CardElement,
   Elements,
@@ -6,6 +6,7 @@ import {
   useStripe,
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import gql from 'graphql-tag';
 import nProgress from 'nprogress';
 import { useState } from 'react';
 import styled from 'styled-components';
@@ -20,6 +21,20 @@ const CheckoutFormStyles = styled.form`
   gap: 1rem;
 `;
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    checkout(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        name
+      }
+    }
+  }
+`;
+
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
 function CheckoutForm() {
@@ -27,7 +42,9 @@ function CheckoutForm() {
   const [loading, setLoading] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
-
+  const [checkout, { error: GraphQLError }] = useMutation(
+    CREATE_ORDER_MUTATION
+  );
   async function handleSubmit(e) {
     // 1. Stop the form from submitting and turn the loader on
     e.preventDefault();
@@ -44,8 +61,17 @@ function CheckoutForm() {
     // 4. Handle any errors from stripe
     if (error) {
       setError(error);
+      nProgress.done();
+      return; // stops the checkout from happening
     }
     // 5. Send the token from #3 to our keystone server, via a custom mutation!
+    const order = await checkout({
+      variables: {
+        token: paymentMethod.id,
+      },
+    });
+    console.log('Finished with the order!!!');
+    console.log(order);
     // 6. Change the page to view the order
     // 7. Close the cart
 
@@ -57,6 +83,7 @@ function CheckoutForm() {
   return (
     <CheckoutFormStyles onSubmit={handleSubmit}>
       {error && <p style={{ fontSize: 12 }}>{error.message}</p>}
+      {GraphQLError && <p style={{ fontSize: 12 }}>{GraphQLError.message}</p>}
       <CardElement />
       <SickButton>Check Out Now</SickButton>
     </CheckoutFormStyles>
