75c723d7d73c712f7b02f033c5b44252807582e8
jmblalock <justin.m.blalock@gmail.com>
2021-05-24 18:54:34 -0500

    created the Order and OrderItems in the Mutuation

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -71,7 +71,31 @@ async function checkout(
     });
   console.log(charge);
   // 4. Convert the cartItems to OrderItems
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.price,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+    return orderItem;
+  });
   // 5. Create the order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+  });
+  // 6. Clean up any old cart items
+  const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds,
+  });
+  return order;
 }
 
 export default checkout;
--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "de3050313afc562f5b0e";
+/******/ 	var hotCurrentHash = "6bc973268b6f35becac5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
